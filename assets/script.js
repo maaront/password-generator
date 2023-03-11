9be92bdf1faa6f3ee67e7b8e7e211e63df89dbd4
maaront <35611834+maaront@users.noreply.github.com>
2023-03-11 10:25:21 -0500

    add alert to ensure at least one character tyep was selected. Add shuffle at tend to better secure password.

--- assets/script.js
+++ assets/script.js
@@ -2,105 +2,102 @@
 var generateBtn = document.querySelector("#generate");
 
 // Assign possible characters
-upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
-specialChars = '!@#$%^&*()';
-numbers = '0123456789';
+let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+let specialChars = "!@#$%^&*()";
+let numbers = "0123456789";
 
 // Generate password based on options
 function generatePassword() {
-
   // Store length of password from user input
-var length = prompt("How many characters would you like your password to be?");
-// Reject any password under 8 in length and start over
-if (length < 8) {
-  alert("Password must be at least 8 characters");
-  generatePassword();
-}
-// Reject andy password over 128 in length and start over
-else if (length > 128) {
-  alert("Password must be no more than 128 characters")
-  generatePassword();
-}
-
-// Ask and store follow up questions for password
-var hasUpperCase = confirm("Would you like Upper Case characters?");
-var hasLowerCase = confirm("Would you like Lower Case characters?");
-var hasNumbers = confirm("Would you like Numbers?");
-var hasSpecialCharacters = confirm("Would you like Special Characters?");
-
-// Test to ensure each input was gathered correctly
-console.log(length, hasUpperCase, hasLowerCase, hasNumbers, hasSpecialCharacters);
-
-/*
- // Add chosen characters to the pool of available characters to generate password from
- if (hasUpperCase) {
-  characters += upperCaseChars;
-}
-if (hasLowerCase) {
-  characters += lowerCaseChars;
-}
-if (hasNumbers) {
-  characters += numbers;
-}
-if (hasSpecialCharacters) {
-  characters += specialChars;
-}
-
-// Write values to console
-console.log(length, hasUpperCase, hasLowerCase, hasNumbers, hasSpecialCharacters);
-
-// Randomly choose list of characters from character pool based on length chosen
-var result = '';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  var length = prompt(
+    "How many characters would you like your password to be?"
+  );
+  // Reject any password under 8 in length and start over
+  if (length < 8) {
+    alert("Password must be at least 8 characters");
+    generatePassword();
   }
-  */
+  // Reject andy password over 128 in length and start over
+  else if (length > 128) {
+    alert("Password must be no more than 128 characters");
+    generatePassword();
+  }
 
-// Ensure password includes at least one character from each type
-// Declare variable "chars" and define as empty
-let chars = '';
-// If hasSpecialCharacters is "true" then randomly assign one character to the "chars"
-if (hasSpecialCharacters) {
-  chars += specialChars.charAt(Math.floor(Math.random() * specialChars.length));
-}
-// If hasUpperCase is "true" then randomly assign one character to the "chars"
-if (hasUpperCase) {
-  chars += upperCaseChars.charAt(Math.floor(Math.random() * upperCaseChars.length));
-}
-// If hasLowerCase is "true" then randomly assign one character to the "chars"
-if (hasLowerCase) {
-  chars += lowerCaseChars.charAt(Math.floor(Math.random() * lowerCaseChars.length));
-}
-// If hasNumbers is "true" then randomly assign one character to "chars"
-if (hasNumbers) {
-  chars += numbers.charAt(Math.floor(Math.random() * numbers.length));
-}
+  // Ask and store follow up questions for password
+  var hasUpperCase = confirm("Would you like Upper Case characters?");
+  var hasLowerCase = confirm("Would you like Lower Case characters?");
+  var hasNumbers = confirm("Would you like Numbers?");
+  var hasSpecialCharacters = confirm("Would you like Special Characters?");
 
-// Test to ensure one of each character-type is added to variable chars
-console.log(chars);
+  // Ensure at least one character type is selected. If not alert user and restart input selection.
+  if (!hasUpperCase && !hasLowerCase && !hasNumbers && !hasSpecialCharacters) {
+    alert("You must choose at least one character type!");
+    generatePassword();
+  }
 
-// Add the rest of the allowable characters to the chars pool
+  // Define variables for each character type and initialize to a random character
+  var upperCaseChar = hasUpperCase
+    ? upperCaseChars.charAt(Math.floor(Math.random() * upperCaseChars.length))
+    : "";
+  var lowerCaseChar = hasLowerCase
+    ? lowerCaseChars.charAt(Math.floor(Math.random() * lowerCaseChars.length))
+    : "";
+  var numberChar = hasNumbers
+    ? numbers.charAt(Math.floor(Math.random() * numbers.length))
+    : "";
+  var specialChar = hasSpecialCharacters
+    ? specialChars.charAt(Math.floor(Math.random() * specialChars.length))
+    : "";
 
+  let approvedChars = upperCaseChar + lowerCaseChar + numberChar + specialChar;
 
-// Generate remaining characters. Define "i" as whatever length result is based off what individual characters were added above
-// Then, as long as i is less than the chosen length of password, add a random character from the entire character pool
-for (let i = chars.length; i < length; i++) {
-  chars += chars.charAt(Math.floor(Math.random() * chars.length));
-}
+  // Add the rest of the allowable characters to the chars pool
+  let characters = "";
 
-// Test to see if chars was populated by more allowable characters to the length defined by user
-console.log(chars);
+  if (hasUpperCase) {
+    characters += upperCaseChars;
+  }
+  if (hasLowerCase) {
+    characters += lowerCaseChars;
+  }
+  if (hasNumbers) {
+    characters += numbers;
+  }
+  if (hasSpecialCharacters) {
+    characters += specialChars;
+  }
 
-// Now shuffle password characters
-chars = chars.split("").sort(() => Math.random() - 0.5).join("");
+  // Subtract length of user input by length of approved characters
+  // This is to ensure at least one of each character type is chosen, and then those are stored for later calculation
+  length = length - approvedChars.length;
 
-//Test to see if result got suffled
-console.log(chars);
+  // Randomly choose list of characters from character pool based on length chosen (minus the rule above)
+  var result = "";
+  var resultLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * resultLength));
+  }
 
-// Return the shuffled result
-  return chars;
+  // Generate remaining characters. Define "i" as whatever length result is based off what individual characters were added above
+  // Then, as long as i is less than the chosen length of password, add a random character from the entire character pool
+  for (let i = characters.length; i < length; i++) {
+    characters += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
+  // Now add the characters we gathered to ensure at least one character type was chosen, and add them to the rest of the characters
+  result = approvedChars + result;
+
+  // Now shuffle password characters
+  result = result
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
+
+  // Return the shuffled result
+  return result;
 }
 
 // Write password to the #password input
@@ -109,7 +106,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 //Add event listener to generate button
